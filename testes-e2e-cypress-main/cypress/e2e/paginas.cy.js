36f31b4608fcf04be132e381e8865827b5b6d280
Natalia <natalia.casarregio@gmail.com>
2024-08-23 15:17:11 -0300

    teste de redirecionamento de paginas

--- testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
@@ -5,24 +5,28 @@ describe('Testando múltiplas páginas', () => {
         cy.getByData('email-input').type('neilton@alura.com')
         cy.getByData('senha-input').type('123456')
         cy.getByData('botao-enviar').click()
-
+        cy.location('pathname').should('eq','/home')
 
     })
     it('Deve acessar a pagina meus cartões', () => {
         cy.getByData('app-home').find('a').eq(1).click()
         cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+        cy.location('pathname').should('eq','/home/cartoes')
     })
     it('Deve acessar a pagina serviços', () => {
         cy.getByData('app-home').find('a').eq(2).click()
         cy.getByData('servicos').should('exist').and('contain', 'Pix')
+        cy.location('pathname').should('eq','/home/servicos')
     })
     it('Deve acessar a pagina investimento', () => {
         cy.getByData('app-home').find('a').eq(3).click()
         cy.getByData('investimento').should('exist').and('have.text', 'Estatísticas')
+        cy.location('pathname').should('eq','/home/investimentos')
     })
     it('Deve acessar a pagina inicio', () => {
         cy.getByData('app-home').find('a').eq(0).click()
         cy.getByData('inicio').should('exist').and('have.text', 'Valor')
+        cy.location('pathname').should('eq','/home')
     })
 
 }) 
