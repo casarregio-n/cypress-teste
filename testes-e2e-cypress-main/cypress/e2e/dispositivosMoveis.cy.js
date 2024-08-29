214b798339d0fad9273d517ed16b2f2eb53abb2f
Natalia <natalia.casarregio@gmail.com>
2024-08-29 11:25:34 -0300

    Foi realizado teste com o tamanho da tela e a direção dela

--- testes-e2e-cypress-main/cypress/e2e/dispositivosMoveis.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/dispositivosMoveis.cy.js
@@ -1,17 +1,70 @@
 describe('Testando dispositivos móveis', () => {
+    beforeEach(() => {
+        cy.viewport(550, 750)
+        cy.viewport('iphone-6')
+    })
     it('Deve existir um menu burguer', () => {
+
         cy.visit('/')
-        
+
         cy.getByData('botao-login').click()
         cy.getByData('email-input').type('neilton@alura.com')
         cy.getByData('senha-input').type('123456')
         cy.getByData('botao-enviar').click()
-        
-        cy.location('pathname').should('eq','/home')
+
+        cy.location('pathname').should('eq', '/home')
 
         cy.getByData('menu-burguer').click()
-        cy.getByData('menu-lateral').find('a').eq(3).click()			
+        cy.getByData('menu-lateral').find('a').eq(3).click()
 
-        cy.location('pathname').should('eq','/home/investimentos')			
+        cy.location('pathname').should('eq', '/home/investimentos')
+    })
+})
+
+describe('Menu de navegação', () => {
+    context('Resolução de 720p', () => {
+        beforeEach(() => {
+            cy.viewport(1280, 720)
+        })
+        it('Deve existir um menu burguer', () => {
+            cy.visit('/')
+
+            cy.getByData('botao-login').click()
+            cy.getByData('email-input').type('neilton@alura.com')
+            cy.getByData('senha-input').type('123456')
+            cy.getByData('botao-enviar').click()
+
+            cy.location('pathname').should('eq', '/home')
+
+            cy.getByData('menu-burguer').click()
+            cy.getByData('menu-lateral').find('a').eq(3).click()
+
+            cy.location('pathname').should('eq', '/home/investimentos')
+
+        })
+    })
+
+    context('Resolução do iphone-5 ', () => {
+        beforeEach(() => {
+            cy.viewport('iphone-5')
+        })
+        it('Deve existir um menu burguer', () => {
+            cy.visit('/')
+
+            cy.getByData('botao-login').click()
+            cy.getByData('email-input').type('neilton@alura.com')
+            cy.getByData('senha-input').type('123456')
+            cy.getByData('botao-enviar').click()
+
+            cy.location('pathname').should('eq', '/home')
+
+            cy.getByData('menu-burguer').click()
+            cy.getByData('menu-lateral').find('a').eq(3).click()
+
+            cy.location('pathname').should('eq', '/home/investimentos')
+
+            cy.viewport('iphone-6', 'landscape')
+
+        })
     })
 })
